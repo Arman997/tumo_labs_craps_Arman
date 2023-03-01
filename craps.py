baa7a79e01785fd81407ff76fd7c1db90923ab54
Arman997 <99550401+Arman997@users.noreply.github.com>
2023-03-01 21:23:55 +0400

    fixed bugs

--- craps.py
+++ craps.py
@@ -1,10 +1,10 @@
-# Game craps
 import random
 
-one = random.randint(1,7)
-two = random.randint(1,7)
+one = random.randint(1,6)
+two = random.randint(1,6)
 craps_sum = one + two
-i = 3
+sum_goal = 0
+i = 7
 print(f'The sum of dice is {one} + {two} = {craps_sum}')
 if craps_sum == 11 or craps_sum == 7:
     print("You won")
@@ -12,15 +12,15 @@ elif craps_sum == 2 or craps_sum == 3 or craps_sum == 12:
     print("Casino wins")
 else:
     print(f'Now your goal number is {craps_sum}')
-    while i != 0:
-        one1 = random.randint(1,7)
-        two2 = random.randint(1,7)
+    player_won = False # added flag variable
+    while i != sum_goal:
+        one1 = random.randint(1,6)
+        two2 = random.randint(1,6)
         sum_goal = one1 + two2
         print(f'The sum of dice is {one1} + {two2} = {sum_goal}')
         if craps_sum == sum_goal:
             print("You won")
-            raise SystemExit
-        i -= 1
-    print("You lose")
-
-
+            player_won = True 
+            break
+    if not player_won: 
+        print("You lose")
